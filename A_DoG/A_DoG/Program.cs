1ee9425929a3ebc4e4891f41ccbebd3443524c0b
RomanGudyak <Gundyak88@mail.ru>
2019-01-27 22:33:51 +0200

    A_Dog

---
+++ A_DoG/A_DoG/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace A_DoG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 60;
+            int y = 15;
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(x, y);
+            Console.Write('@');
+            ConsoleKey key;
+            while ((key = Console.ReadKey().Key) != ConsoleKey.Escape)
+            {
+                switch (key)
+                {
+                    case ConsoleKey.RightArrow:
+                        x = x + 1;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        x = x - 1;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        y = y - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        y = y + 1;
+                        break;
+                    default:
+                        Console.Beep();
+                        break;
+                }
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                Console.Write("@");
+            }
+        }
+    }
+}
