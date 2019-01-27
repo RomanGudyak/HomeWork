9a6f07928a05cb01ef444d7518c41d156f8a578a
RomanGudyak <Gundyak88@mail.ru>
2019-01-27 22:24:24 +0200

    HomeWork

---
+++ Math/Math/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Простейшие математические вычисления для двух переменных");
+            Console.Write("Введите первое число:");
+            Double a, b;
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"a={a}");
+            Console.Write("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"b={b}");
+            Double add = a + b;
+            Double sub = a - b;
+            Double mult = a * b;
+            Double div = a / b;
+            Console.WriteLine($"\na+b={add}, a-b={sub}, a*b={mult}, a/b={div.ToString("F4")}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pifagore/Pifagore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pifagore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблица ПИФАГОРА");
+            int[] line1 = new int[9];
+            int[] line2 = new int[9];
+            string Tabl = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                line1[i] = i + 1;
+                line2[i] = i + 1;
+            }
+
+
+            for (int i = 0; i < line2.Length; i++)
+            {
+                for (int z = 0; z < line1.Length; z++)
+                {
+                    Tabl += "\t" + line2[i] * line1[z];
+                }
+                Tabl += "\n";
+            }
+
+            Console.WriteLine(Tabl);
+
+            Console.ReadKey();
+        }
+    }
+}
