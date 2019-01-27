c882df08c1886cac403df3d2d1ffe361a5c041bb
RomanGudyak <RomanGundyak88@mail.ru>
2019-01-27 12:45:57 +0200

    Таблица Пифагора

---
+++ Pifagor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pifagore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\tТаблица ПИФАГОРА размерностью X на У");
+            Console.Write("Введите X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x * y != 0 && x>0 && y>0)
+            {
+                for (int m = 1; m <= y; m++)
+                {
+                    for (int n = 1; n <= x; n++)
+                    {
+                        int res = n * m;
+                        Console.Write("{0, 5}", res);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("Такую таблицу нам не заказывали:)");
+            Console.ReadKey();
+        }
+    }
+}
